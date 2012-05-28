43c7c090ba38dd4140c94ff2f1ef04322e1044a5
steventux <steve.laing@gmail.com>
2012-05-28 22:44:01 +0100

    Fixed README

--- test/test_minitest_activerecord_assertions.rb
+++ test/test_minitest_activerecord_assertions.rb
@@ -1,5 +1,6 @@
 require 'helper'
 
+
 class TestMinitestActiveRecordAssertions < Test::Unit::TestCase
   should "probably rename this file and start testing for real" do
     flunk "hey buddy, you should probably rename this file and start testing for real"
